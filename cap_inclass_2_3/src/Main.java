7bb55a166469b2d673f16d4f357f5bcf02fa77be
Apoz626 <133711643+Apoz626@users.noreply.github.com>
2025-02-03 09:48:44 -0600

    Committed branch change

--- cap_inclass_2_3/src/Main.java
+++ cap_inclass_2_3/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         int x = 1; // changed in origin
         System.out.println("Hello world!");
+        System.out.println("This is a branch change"); // added in WorkOnFeatureABC123
     }
 }
